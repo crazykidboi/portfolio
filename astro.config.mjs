fcf2584d9df796c0e0a5545aca1ae554c4c2182c
Liam Lyons <electriclego104@gmail.com>
2025-09-04 17:13:44 -0400

    evrf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://lyonsiv.mov',
   base: '/',
-  output: 'static',     // MUST be static
+  output: 'static',       // MUST be 'static'
   integrations: [react()],
   vite: { plugins: [tailwindcss()] },
+  experimental: {
+    fonts: [{
+      provider: fontProviders.google(),
+      name: "Geist",
+      cssVariable: "--font-geist",
+      fallbacks: ["Inter", "sans-serif"],
+    }]
+  }
 });
