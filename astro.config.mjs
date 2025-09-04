06f674e640a3e5d5d646f80c37bfbf952e8124f9
Liam Lyons <electriclego104@gmail.com>
2025-09-04 17:42:53 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
+
+// REMOVED the import for the missing "remark-reading-time.mjs" file.
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://lyonsiv.mov",
   integrations: [tailwind(), react()],
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-  },
-  // VERCEL
-  // The error is caused by the line below.
-  // 'server' mode is for on-demand rendering and requires an adapter.
-  // To build a static site, you need to change this.
-  // CHANGE THIS LINE:
-  // output: "server",
-  // TO THIS LINE:
+  // REMOVED the markdown section that was trying to use the missing plugin.
   output: "static",
 });
