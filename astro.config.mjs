faa3d820587028b44c414cb8cb3265ef36544622
Liam Lyons <electriclego104@gmail.com>
2026-02-27 00:21:30 -0500

    gggggg

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 
-import vercel from "@astrojs/vercel";
-
-
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://crazykidboi.github.io',
