950fb0b896cef3b03adb8bfba5d93b47eccb3315
Liam Lyons <electriclego104@gmail.com>
2026-02-28 02:09:07 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lyonsiv.mov',
+  site: 'https://www.lyonsiv.mov',
   
   
   output: 'static',
