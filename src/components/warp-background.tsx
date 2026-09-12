32df9d55b5ee0e828ac9582f60c412671cc7e837
Liam Lyons <electriclego104@gmail.com>
2026-09-11 23:51:08 -0400

    a lot
    pnpm update, fixing a lot of small UI things sep 11, a lot of astro conponents and padding changes

--- src/components/warp-background.tsx
+++ src/components/warp-background.tsx
@@ -9,6 +9,7 @@ export default function WarpBackground(props: WarpProps) {
         rotation: 0.4,
         style: { width: '100%', height: '100%' }
         
+        
     };
 
     return <Warp {...defaultProps} {...props} style={{ ...defaultProps.style, ...props.style }} />;
